848a20fcf51ca4ec7826c9ae98908ee13ded4c46
meghna192 <87377289+meghna192@users.noreply.github.com>
2022-01-20 21:37:14 +0530

    Add files via upload

---
+++ Permutation Sequence.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def getPermutation(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        seq, k, fact = "", k - 1, math.factorial(n - 1)
+        perm = [i for i in xrange(1, n + 1)]
+        for i in reversed(xrange(n)):
+            curr = perm[k / fact]
+            seq += str(curr)
+            perm.remove(curr)
+            if i > 0:
+                k %= fact
+                fact /= i
+        return seq
+
+    
+if __name__ == "__main__":
+    print Solution().getPermutation(3, 2)
+        
